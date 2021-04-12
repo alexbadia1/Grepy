39fb4deb11c4bf192032373829f5f8f5bd8b1d9f
Alex Badia <badia.alex.h@gmail.com>
2021-04-12 00:23:21 -0400

    Testing commit to main

--- src/Grepy.java
+++ src/Grepy.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Grepy {
 	public static void main (String[] args) {
-		Scanner myObj = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		
 		System.out.print("Regular Expression: ");
 		
-		String regex = myObj.nextLine();
+		String regex = scanner.nextLine();
 		System.out.println();
 		Lexer lexer = new Lexer();
 		lexer.lex(regex);
